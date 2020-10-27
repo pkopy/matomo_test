ef2878115a946fcaaeeded93cfcd4677acff7174
Paweł Kopycki <pawel.kopycki@qed.pl>
2020-10-27 11:55:19 +0100

    start

--- src/index.js
+++ src/index.js
@@ -3,10 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react'
 
+const instance = createInstance({
+    urlBase: 'http://matomo.qed:8099/matomo/matomo.php',
+    siteId: 3,
+    // userId: 'UID76903202', // optional, default value: `undefined`.
+    // trackerUrl: 'https://LINK.TO.DOMAIN/tracking.php', // optional, default value: `${urlBase}matomo.php`
+    // srcUrl: 'https://LINK.TO.DOMAIN/tracking.js', // optional, default value: `${urlBase}matomo.js`
+    disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
+    heartBeat: { // optional, enabled by default
+        active: true, // optional, default value: true
+        seconds: 10 // optional, default value: `15
+    },
+    linkTracking: false, // optional, default value: true
+    configurations: { // optional, default value: {}
+        // any valid matomo configuration, all below are optional
+        disableCookies: true,
+        setSecureCookie: true,
+        setRequestMethod: 'POST'
+    }
+})
 ReactDOM.render(
   <React.StrictMode>
+      <MatomoProvider value={instance}>
     <App />
+      </MatomoProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
